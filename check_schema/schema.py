ad1fa0c911241d5599ab9a65ecc097962f996161
zqmillet <zqmillet@qq.com>
2021-08-09 00:16:36 +0800

    update

--- check_schema/schema.py
+++ check_schema/schema.py
@@ -1,6 +1,8 @@
 import yaml
 import importlib
 
+from check_schema.exceptions import InitializeLambdaExpressionException
+
 class Schema(dict):
     type_map = {
         'int': int,
@@ -16,18 +18,23 @@ class Schema(dict):
 
     def __init__(self, configuration):
         super().__init__(configuration)
-        self._convert_type()
+        self._handle_type()
+        self._handle_assertion()
+        self._handle_properties()
+        self._handle_items()
 
+    def _handle_properties(self):
         properties = self.get('properties', dict())
         for name, _configuration in properties.items():
             properties[name] = Schema(_configuration)
 
+    def _handle_items(self):
         if isinstance(self.get('items'), dict):
             self['items'] = Schema(self['items'])
         elif isinstance(self.get('items'), list):
             self['items'] = [Schema(item) for item in self['items']]
 
-    def _convert_type(self):
+    def _handle_type(self):
         _type = self['type']
         if isinstance(_type, str):
             types = [_type]
@@ -35,6 +42,18 @@ class Schema(dict):
             types = _type
 
         self['type'] = tuple([self._load_type(_type) for _type in types])
+
+    def _handle_assertion(self):
+        expression = self.get('assertion')
+        if not expression:
+            return
+
+        try:
+            lambda_expression = eval(expression) # pylint: disable = eval-used
+        except Exception:
+            raise InitializeLambdaExpressionException(expression) from None
+        
+        self['assertion'] = lambda_expression
 
     def _load_type(self, _type):
         _type = str(_type)
--- testcases/load_schema_test.py
+++ testcases/load_schema_test.py
@@ -7,8 +7,10 @@ def test_load_schema():
     properties:
         x:
             type: int
+            assertion: "lambda x: x > 0"
         y:
             type: int
+            assertion: "lambda x: x >= 0"
         person:
             type: list
             items:
@@ -18,13 +20,15 @@ def test_load_schema():
                         type: str
                     age:
                         type: [int, None]
+                        assertion: "lambda x: x is None"
         array:
             type: list
             items:
                 - type: int
                 - type: float
+                  assertion: "lambda x: x ** 2 < 100"
                 - type: [str, null]
-        
+    assertion: "lambda x: len(x) > 0"
     '''
     schema = load_schema(string)
     assert schema['type'] == (dict,)
@@ -38,3 +42,9 @@ def test_load_schema():
     assert schema['properties']['array']['items'][0]['type'] == (int,)
     assert schema['properties']['array']['items'][1]['type'] == (float,)
     assert schema['properties']['array']['items'][2]['type'] == (str, type(None))
+
+    assert callable(schema['assertion'])
+    assert callable(schema['properties']['x']['assertion'])
+    assert callable(schema['properties']['y']['assertion'])
+    assert callable(schema['properties']['array']['items'][1]['assertion'])
+    assert callable(schema['properties']['person']['items']['properties']['age']['assertion'])
